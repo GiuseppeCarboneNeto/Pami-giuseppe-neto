2fdb37ca50cac8f917855708403c374927cf2e21
giuseppecarboneneto <zaperzipor@gmail.com>
2025-03-24 07:34:58 -0300

    Aulas de pami

---
+++ Aula02/variaveis.js
@@ -0,0 +1,17 @@
+var numero = 2;
+let texto = "HelloWord";
+const oQueEisso = "?";
+
+console.log('Tipo var :', numero);
+
+numero = 13;
+
+console.log(texto + " o numero é "+ numero);
+
+numero = 4;
+
+console.log('$(texto) e o numero agora é $(numero)')
+
+texto = 69;
+
+console.log(texto + numero);
